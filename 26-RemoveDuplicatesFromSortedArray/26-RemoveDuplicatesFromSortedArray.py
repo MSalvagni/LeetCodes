d35c040c7a318d5e05386cb183afaa1d27c1616f
GMNMGN <megansalvagni@gmail.com>
2025-11-03 16:48:08 -0500

    Create 26-RemoveDuplicatesFromSortedArray

---
+++ 26-RemoveDuplicatesFromSortedArray/26-RemoveDuplicatesFromSortedArray.py
@@ -0,0 +1,21 @@
+# Last updated: 11/3/2025, 4:48:08 PM
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+
+        unique_nums = 0
+
+        for i in range(1, len(nums)):
+            if nums[i] != nums[unique_nums]:
+                unique_nums += 1 
+                nums[unique_nums] = nums[i]
+        return unique_nums + 1
+
+
+
+
